d36101651813cf1b132c847a13b806c76c7b8a17
Winston Muijs <winstonhmuijs@gmail.com>
2022-09-23 13:32:03 +0200

    Updated mani-view.jsx with axios
    To fetch array movies in mainview component with axios through componentDidMount method

--- src/components/mainview/main-view.jsx
+++ src/components/mainview/main-view.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import axios from 'axios';
+
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
+import { response } from 'express';
 
 
 
@@ -9,14 +12,18 @@ export class MainView extends React.Component {
     constructor(){
         super();
         this.state = {
-            movies: [
-                { _id: 1, title: 'The Fountain', description: ' As a modern-day scientist, Tommy is struggling with mortality, desperately searching for the medical breakthrough that will save the life of his cancer-stricken wife, Izzi.', genre: ' Drama', director: ' Darren Aronofski', imageURL: 'https://m.media-amazon.com/images/I/51Ja7PFr9eL.jpg' },
-                { _id: 2, title: 'The Eternal Sunshine of the Spotless Mind', description: ' When their relationship turns sour, a couple undergoes a medical procedure to have each other erased from their memories.', genre: ' Drama', director: ' Michel Goundry', imageURL: 'https://m.media-amazon.com/images/M/MV5BZTg3ODg2MzMtZmRmYy00ZWUwLTk5Y2QtOThmOTY1ZWZjZmJlXkEyXkFqcGdeQXVyODIyOTEyMzY@._V1_.jpg' },
-                { _id: 3, title: 'The Tree of Life', description: ' The story of a family in Waco, Texas in 1956. The eldest son witnesses the loss of innocence and struggles with his parents conflicting teachings.', genre: ' Drama', director: ' Terrence Malick', imageURL:'https://m.media-amazon.com/images/I/717aO-A7McL._AC_SL1500_.jpg'}
-
-            ],
+            movies: [],
             selectedMovie: null
         }
+    }
+
+    componentDidMount(){
+      axios.get('https://arthousemovie.herokuapp.com/movies').then(response => {
+        this.setState({
+          movies: response.data
+        });
+      }).catch(error => {
+        console.log(error)});
     }
 
     setSelectedMovie(newSelectedMovie) {
@@ -25,22 +32,22 @@ export class MainView extends React.Component {
       });
     }
   
-    render() {
-      const { movies, selectedMovie } = this.state;
-    
-      if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
-    
-      return (
-        <div className="main-view">
-          {selectedMovie
-            ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
-            : movies.map(movie => (
-              <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }}/>
-            ))
-          }
-        </div>
-      );
-    }
+  render() {
+     const { movies, selectedMovie } = this.state;
+
+     if (movies.length === 0) return <div className="main-view" />;
+
+     return (
+       <div className="main-view">
+         {selectedMovie
+           ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
+           : movies.map(movie => (
+             <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }}/>
+          ))
+         }
+       </div>
+     );
+  }
     
 }
 
