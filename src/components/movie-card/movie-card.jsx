48b6d084f58826080f68febb352e2a895bb55c52
Winston Muijs <winstonhmuijs@gmail.com>
2022-10-03 13:40:38 +0200

    Updated the movie-card with links

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,27 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Button, Row, Col, Container} from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+
+import { Link } from "react-router-dom";
+import { CardImg } from 'react-bootstrap';
 
 export class MovieCard extends React.Component {
   render() {
-    const { movie, onMovieClick } = this.props;
+    const { movie } = this.props;
 
     return (
-      <Container>
-      <Row>
-          <Col>
-          <Card className='card'>
-            <Card.Img variant="top" src={movie.ImagePath} />
-            <Card.Body>
-              <Card.Title>{movie.Title}</Card.Title>
-              <Card.Text>{movie.Description}</Card.Text>
-              <Button onClick={() => onMovieClick(movie)} variant="link">{movie.title}</Button>
-            </Card.Body>
-          </Card>
-         </Col>
-      </Row>
-     </Container> 
+      <Card>
+        <Card.Body>
+          <Card.Title>{movie.title}</Card.Title>
+          <Card.Text>{movie.description}</Card.Text>
+          <Link to={`/movies/${movie._id}`}>
+            <Button variant="link"><CardImg src={movie.imageURL} crossOrigin={'anonymous'}
+                /></Button>
+          </Link>
+        </Card.Body>
+      </Card>
     );
   }
 }
@@ -31,8 +30,7 @@ MovieCard.PropTypes = {
     title: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
     imageURL: PropTypes.string.isRequired,
-    genre: PropTypes.number.isRequired,
-    director: PropTypes.number.isRequired
-  }).isRequired,
-  onMovieClick: PropTypes.func.isRequired
+  }).isRequired
 };
+
+
