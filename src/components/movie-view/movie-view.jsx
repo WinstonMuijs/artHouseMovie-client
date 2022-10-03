39618c06949d06944d1e254bf68f75823a0e385b
Winston Muijs <winstonhmuijs@gmail.com>
2022-10-03 14:40:30 +0200

    Updated the link in movie-view

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -39,13 +39,13 @@ export class MovieView extends React.Component {
                 <span className='value'>{movie.genre}</span>
               </Col>
             </Row>
-            {/* <Row>
-              <Col className='movie-genre'>
-                <Link to={`/genres/${movie.genre._id}`}>
-                  <Button variant="link">Genre</Button>
+            <Row>
+              <Col className='movie-director'>
+                <Link to={`/directors/${director._id}`}>
+                  <Button variant="link">Director</Button>
                 </Link>
               </Col>
-            </Row> */}
+            </Row>
 
             <Row>
               <Col>
