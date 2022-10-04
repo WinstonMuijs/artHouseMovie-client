49411f65c9f9cd66edfcd6777baca36b658275ce
Winston Muijs <winstonhmuijs@gmail.com>
2022-10-04 09:50:47 +0200

    Updated DirectorCard component

---
+++ src/components/director-card/director-card.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+
+export class DirectorCard extends React.Component {
+  render() {
+    const { director } = this.props;
+
+    return (
+      <Card>
+      <Card.Body>
+          <Card.Title>{director._id}</Card.Title>
+          <Card.Text>{director.name}</Card.Text>
+          <Card.Text>{director.bio}</Card.Text>
+          <Card.Text>{director.birthyear}</Card.Text>
+          <Link to={`/directors/${director._id}`}>
+            <Button variant="link">Open</Button>
+          </Link>
+        </Card.Body>
+      </Card>
+    );
+  }
+}
+
+DirectorCard.PropTypes = {
+  director: PropTypes.shape({
+    _id: PropTypes.number,
+    name: PropTypes.string.isRequired,
+    bio: PropTypes.string.isRequired,
+    birthyear: PropTypes.string,
+    deathyear: PropTypes.string
+  }).isRequired
+};
