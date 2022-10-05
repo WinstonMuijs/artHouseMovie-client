f7ecd6a73c4b9efb9d3789ba92a5fb60108e3e75
Winston Muijs <winstonhmuijs@gmail.com>
2022-10-05 09:28:57 +0200

    Created a genre-card Component

---
+++ src/components/genre-card/genre-card.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+import { Link } from 'react-router-dom';
+
+export class GenreCard extends React.Component {
+  render() {
+    const { genre } = this.props;
+
+    return (
+      <Card>
+      <Card.Body>
+          <Card.Title>{genre._id}</Card.Title>
+          <Card.Text>{genre.name}</Card.Text>
+          <Card.Text>{genre.description}</Card.Text>
+          <Link to={`/genres/${genre._id}`}>
+            <Button variant="link">Open</Button>
+          </Link>
+        </Card.Body>
+      </Card>
+    );
+  }
+}
+
+GenreCard.PropTypes = {
+  genre: PropTypes.shape({
+    _id: PropTypes.number,
+    name: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired
+  }).isRequired
+};
