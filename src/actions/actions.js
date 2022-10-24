cbf6ebd915e49db08e4f48331f79f6860f155b5e
Winston Muijs <winstonhmuijs@gmail.com>
2022-10-24 15:46:00 +0200

    Updated the actons

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,10 +1,10 @@
 export const SET_MOVIES = 'SET_MOVIES';
 export const SET_FILTER = 'SET_FILTER';
-export const CREATE_USER = 'CREATE_USER';
-export const UPDATE_USER = 'UPDATE_USER';
-export const DELETE_USER = 'DELETE_USER';
-export const SET_FAVMOVIE = 'SET_FAVMOVIE';
-export const REMOVE_FAVMOVIE = 'REMOVE_FAVMOVIE';
+// export const CREATE_USER = 'CREATE_USER';
+// export const UPDATE_USER = 'UPDATE_USER';
+// export const DELETE_USER = 'DELETE_USER';
+// export const SET_FAVMOVIE = 'SET_FAVMOVIE';
+// export const REMOVE_FAVMOVIE = 'REMOVE_FAVMOVIE';
 
 
 export function setMovies(value) {
@@ -15,22 +15,22 @@ export function setFilter(value) {
     return { type: SET_FILTER, value };
 }
 
-export function createUser(text) {
-    return { type: CREATE_USER, text };
-}
+// export function createUser(text) {
+//     return { type: CREATE_USER, text };
+// }
 
-export function updateUser(text) {
-    return { type: UPDATE_USER, text };
-}
+// export function updateUser(text) {
+//     return { type: UPDATE_USER, text };
+// }
 
-export function deleteUser(id) {
-    return { type: DELETE_USER, id };
-}
+// export function deleteUser(id) {
+//     return { type: DELETE_USER, id };
+// }
 
-export function setFavmovie(index) {
-    return { type: SET_FAVMOVIE, index };
-}
+// export function setFavmovie(index) {
+//     return { type: SET_FAVMOVIE, index };
+// }
 
-export function removeFavmovie(index) {
-    return { type: REMOVE_FAVMOVIE, index };
-}
+// export function removeFavmovie(index) {
+//     return { type: REMOVE_FAVMOVIE, index };
+// }
