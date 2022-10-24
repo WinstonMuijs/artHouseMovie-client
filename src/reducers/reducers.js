fbcf6b4a7ec6beb7bd7c412867ecbbc25eb78783
Winston Muijs <winstonhmuijs@gmail.com>
2022-10-24 15:43:03 +0200

    Updated the reducers

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,6 +1,6 @@
 import { combineReducers } from "redux";
 
-import { SET_FILTER, SET_MOVIES, CREATE_USER, UPDATE_USER, DELETE_USER, SET_FAVMOVIE, REMOVE_FAVMOVIE} from "../actions/actions";
+import { SET_FILTER, SET_MOVIES } from "../actions/actions";
 
 function visibilityFilter(state = '', action) {
     switch (action.type) {
@@ -20,62 +20,62 @@ function movies(state = [], action){
     }
 }
 
-function users(state={}, action) {
-    switch(action.type) {
-        case CREATE_USER:
-            return {
-                ...state,
-                name: action.name,
-                password: action.password,
-                email: action.email,
-                birthday: action.birthday,
-                };
+// function users(state={}, action) {
+//     switch(action.type) {
+//         case CREATE_USER:
+//             return {
+//                 ...state,
+//                 name: action.name,
+//                 password: action.password,
+//                 email: action.email,
+//                 birthday: action.birthday,
+//                 };
         
-        case UPDATE_USER: 
-                return {
-                    ...state,
-                  name: action.name,
-                  password: action.password,
-                  email: action.email,
-                  birthday: action.birthday,  
-                };
+//         case UPDATE_USER: 
+//                 return {
+//                     ...state,
+//                   name: action.name,
+//                   password: action.password,
+//                   email: action.email,
+//                   birthday: action.birthday,  
+//                 };
         
-        case DELETE_USER: 
-                 return state.map((user, index) => (index===action.index) ? {...user, deleted: !user.deleted} : user)
+//         case DELETE_USER: 
+//                  return state.map((user, index) => (index===action.index) ? {...user, deleted: !user.deleted} : user)
                     
                  
-        default: return state;
+//         default: return state;
 
-    }
-}
+//     }
+// }
 
 
-function favoriteMovies(state=[], action) {
-    switch(action.type) {
-        //add to the array
-        case SET_FAVMOVIE:
-            return {
-                ...state, 
-                favoriteMovies: [...state.favoriteMovies, action.index] 
-            }
-        case REMOVE_FAVMOVIE:
-            //remove from the array
-            return {
-                ...state,
-                favoriteMovies: [...state.favoriteMovies.slice(0, index), state.favoriteMovies.slice(index + 1)]
-            }
+// function favoriteMovies(state=[], action) {
+//     switch(action.type) {
+//         //add to the array
+//         case SET_FAVMOVIE:
+//             return {
+//                 ...state, 
+//                 favoriteMovies: [...state.favoriteMovies, action.index] 
+//             }
+//         case REMOVE_FAVMOVIE:
+//             //remove from the array
+//             return {
+//                 ...state,
+//                 favoriteMovies: [...state.favoriteMovies.slice(0, index), state.favoriteMovies.slice(index + 1)]
+//             }
             
-            default: return state;
+//             default: return state;
 
-    }
-}
+//     }
+// }
 
 
 const moviesApp = combineReducers({
     visibilityFilter,
     movies,
-    users,
-    favoriteMovies
+    // users,
+    // favoriteMovies
 
 })
 
