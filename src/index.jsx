353c527a19f49b322891cc20b26307862cbecf70
Winston Muijs <winstonhmuijs@gmail.com>
2022-10-25 15:40:43 +0200

    Updated the index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Container } from 'react-bootstrap/Container';
-import * as ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import Container from 'react-bootstrap/Container';
 import { legacy_createStore as createStore } from 'redux';
 import { Provider } from 'react-redux';
 import moviesApp from './reducers/reducers';
-import  MainView  from './components/mainview/main-view';
 
-// Import statement to indicate that you need to bundle `./index.scss`
+import MainView from './components/mainview/main-view';
+
+
+// Import statement to indicate that we need to bundle `./index.scss`
 import './index.scss';
 
 const store = createStore(moviesApp);
@@ -14,8 +16,8 @@ const store = createStore(moviesApp);
 // Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
   render() {
-    return (
-      <Provider store={store}>
+    return(
+     <Provider store={store}>
         <Container>
           <MainView />
         </Container>
@@ -25,11 +27,17 @@ class MyFlixApplication extends React.Component {
 }
 
 // Finds the root of your app
-const root = ReactDOM.createRoot(document.getElementsByClassName('app-container')[0]);
+// const root = ReactDOM.createRoot(document.getElementsByClassName('app-container')[0]);
 
 // Tells React to render your app in the root DOM element
-root.render(
-  <React.StrictMode>
-    <MyFlixApplication/>
-  </React.StrictMode>
-);
+// root.render(
+//   <React.StrictMode>
+//     <MyFlixApplication/>
+//   </React.StrictMode>
+// );
+
+// Find the root of our app
+const container = document.getElementsByClassName('app-container')[0];
+
+// Tell React to render our app in the root DOM element
+ReactDOM.render(React.createElement(MyFlixApplication), container);
