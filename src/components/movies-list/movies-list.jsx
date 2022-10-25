9c2b73f4962389393fcb035a63a5aba7d40c48cb
Winston Muijs <winstonhmuijs@gmail.com>
2022-10-25 15:40:57 +0200

    Changed the movie-list

--- src/components/movies-list/movies-list.jsx
+++ src/components/movies-list/movies-list.jsx
@@ -1,37 +1,35 @@
-import React from "react";
-import { Col }  from "react-bootstrap/Col";
-import { connect } from "react-redux";
+import React from 'react';
+import Col from 'react-bootstrap/Col';
+import { connect } from 'react-redux';
 
-// import VisibilityFilterInput from "../visibility-filter-input/visibility-filter-input";
-
-import SearchBar from '../search-bar/search-bar';
-
+import VisibilityFilterInput from '../visibility-filter-input/visibility-filter-input';
 import { MovieCard } from '../movie-card/movie-card';
 
 const mapStateToProps = state => {
-    const { visibilityFilter } = state;
-    return { visibilityFilter };
-}
+  const { visibilityFilter } = state;
+  return { visibilityFilter };
+};
 
 function MoviesList(props) {
-    const { movies, visibilityFilter }=props;
-    let filteredMovies = movies;
+  const { movies, visibilityFilter } = props;
+  let filteredMovies = movies;
 
-    if (visibilityFilter !== '') {
-        filteredMovies = movies.filter(m => m.title.toLowerCase().includes(visibilityFilter.toLowerCase()));
-    }
-    if (!movies) return <div className="main-view" />;
+  if (visibilityFilter !== '') {
+    filteredMovies = movies.filter(m => m.Title.toLowerCase().includes(visibilityFilter.toLowerCase()));
+  }
 
-    return <>
-    <Col md={12} style={{ margin: '1em'}}>
-        <SearchBar visibilityFilter={visibilityFilter} />
+  if (!movies) return <div className="main-view"/>;
+
+  return <>
+    <Col md={12} style={{ margin: '1em' }}>
+      <VisibilityFilterInput visibilityFilter={visibilityFilter} />
     </Col>
     {filteredMovies.map(m => (
-        <Col md={3} key={m._id}>
-            <MovieCard movie={m} />
-        </Col>
+      <Col md={3} key={m._id}>
+        <MovieCard movie={m} />
+      </Col>
     ))}
-      </>
+  </>;
 }
 
-export default connect (mapStateToProps)(MoviesList);
+export default connect(mapStateToProps)(MoviesList);
